84259161bad4d5f106cde7f5cb369fde2872b0a9
Geraldo Eloi <geraldo.eloi11@gmail.com>
2025-03-17 18:52:16 -0300

    feat: custom_auth models

---
+++ backend/custom_auth/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from utils.models import BaseModel
+
+class User(BaseModel):
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=255)
+    role = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True)
+
+class AuthCode(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    code = models.CharField(max_length=6)
+    is_active = models.BooleanField(default=True)
+    expired_at = models.DateTimeField()
