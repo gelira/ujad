a101584b472bc341103ad34b90d1e2c4985e2af0
Geraldo Eloi <geraldo.eloi11@gmail.com>
2025-03-19 21:02:31 -0300

    feat: sales models

--- backend/sales/models.py
+++ backend/sales/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from custom_auth.models import User
+from utils.models import BaseModel
 
-# Create your models here.
+class Wallet(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    is_active = models.BooleanField(default=True)
+    tickets_amount = models.IntegerField(default=0)
+
+class Ticket(BaseModel):
+    wallet = models.ForeignKey(Wallet, on_delete=models.PROTECT)
+    status = models.CharField(max_length=50)
+    original_value = models.IntegerField()
+    remaining_value = models.IntegerField()
+
+class Product(BaseModel):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.IntegerField()
+    quantity = models.IntegerField(default=0)
+
+class ProductTicket(BaseModel):
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    ticket = models.ForeignKey(Ticket, on_delete=models.PROTECT)
+    dispatcher = models.ForeignKey(User, on_delete=models.PROTECT)
+    consumed = models.BooleanField(default=False)
