3a627a6dbaee7d85426f7788ae2f0f0fcaa679cd
Geraldo Eloi <geraldo.eloi11@gmail.com>
2025-03-30 17:15:56 -0300

    feat: user roles

--- backend/custom_auth/models.py
+++ backend/custom_auth/models.py
@@ -5,7 +5,12 @@ from utils import generate_random_numeric_string
 from utils.models import BaseModel
 from uuid import UUID
 
+from custom_auth.exceptions import InvalidAuthCodeException
+
 class User(BaseModel):
+    ROLE_CONSUMER = 'consumer'
+    ROLE_DISPATCHER = 'dispatcher'
+
     email = models.EmailField(unique=True)
     name = models.CharField(max_length=255, blank=True)
     role = models.CharField(max_length=50, blank=True)
@@ -15,7 +20,10 @@ class User(BaseModel):
     def get_or_create_by_email(cls, email: str):
         user, _ = cls.objects.get_or_create(
             email=email,
-            defaults={'name': '', 'role': ''}
+            defaults={
+                'name': '',
+                'role': cls.ROLE_CONSUMER
+            }
         )
 
         return user
@@ -41,7 +49,10 @@ class AuthCode(BaseModel):
             code=code,
             is_active=True,
             expired_at__gt=timezone.now()
-        ).get()
+        ).first()
+
+        if not auth_code:
+            raise InvalidAuthCodeException()
 
         auth_code.is_active = False
         auth_code.save()
