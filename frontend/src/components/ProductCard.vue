b6f3c273580e9e65192741316f936c7b6cb0b779
Geraldo Eloi <geraldo.eloi11@gmail.com>
2025-04-02 19:38:43 -0300

    feat: product cards

---
+++ frontend/src/components/ProductCard.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps<{ product: Product }>()
+</script>
+
+<template>
+  <v-card elevation="2">
+    <v-card-title>{{ product.name }}</v-card-title>
+    <v-card-subtitle>{{ product.quantity }} disponíveis</v-card-subtitle>
+    <v-card-text v-if="!!product.description">
+      {{ product.description }}
+    </v-card-text>
+    <v-card-actions class="justify-space-between">
+      <v-btn icon="mdi-minus"></v-btn>
+      <span>0</span>
+      <v-btn icon="mdi-plus"></v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
--- frontend/src/views/NewOrderView.vue
+++ frontend/src/views/NewOrderView.vue
@@ -1,3 +1,18 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+
+import { useProductStore } from '@/stores/products'
+import ProductCard from '@/components/ProductCard.vue'
+
+const productStore = useProductStore()
+
+onMounted(() => {
+  productStore.getProducts()
+})
+</script>
+
 <template>
-  <h1>New Order</h1>
+  <div class="d-flex flex-column ga-2">
+    <ProductCard v-for="p in productStore.products" :key="p.uid" :product="p" />
+  </div>
 </template>
