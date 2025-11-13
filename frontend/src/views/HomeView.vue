c1ae45944fdffdd56ac4aacaa0e79343e796e9af
Geraldo Eloi <geraldo.eloi11@gmail.com>
2025-11-13 19:11:41 -0300

    feat: clean up home component

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,35 +1,7 @@
 <script setup lang="ts">
-import { onMounted } from 'vue'
-import { RouterView } from 'vue-router'
+import { RouterView } from 'vue-router';
 
-import { useAuthStore } from '@/stores/auth'
-import { useNavigationStore } from '@/stores/navigation'
-import { removeToken } from '@/utils/localStorage'
-import AppBar from '@/components/AppBar.vue'
-import { ROUTES } from '@/router'
-
-const authStore = useAuthStore()
-const navigationStore = useNavigationStore()
-
-onMounted(() => {
-  authStore.getUserInfo()
-    .then(() => {
-      const routeName = navigationStore.activeRoute?.name
-
-      if (
-        routeName === ROUTES.HOME.name ||
-        routeName === ROUTES.CONSUME.name
-      ) {
-        authStore.user.role === 'consumer'
-          ? navigationStore.goToTickets()
-          : navigationStore.goToConsume()
-      }
-    })
-    .catch(() => {
-      removeToken()
-      navigationStore.goToLogin()
-    })
-})
+import AppBar from '@/components/AppBar.vue';
 </script>
 
 <template>
