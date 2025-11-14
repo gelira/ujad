c6bca556cfbbe89f0ed167a4222f3d0127bc394b
Geraldo Eloi <geraldo.eloi11@gmail.com>
2025-11-13 22:38:13 -0300

    feat: automatic redirect on home enter

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,8 +1,30 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router';
+import { watch } from 'vue';
+import { RouterView, useRoute } from 'vue-router';
 
 import AppBar from '@/components/AppBar.vue';
 import UserNameDialog from '@/components/UserNameDialog.vue';
+import { ROUTES } from '@/router';
+import { useAuthStore } from '@/stores/auth';
+import { useNavigationStore } from '@/stores/navigation';
+
+const route = useRoute()
+const authStore = useAuthStore()
+const navigationStore = useNavigationStore()
+
+watch(
+  () => [route.name, authStore.user.role],
+  ([routeName, role]) => {
+    if (routeName !== ROUTES.HOME.name) {
+      return
+    }
+
+    role === 'consumer'
+      ? navigationStore.goToTickets()
+      : navigationStore.goToConsume()
+  },
+  { immediate: true }
+)
 </script>
 
 <template>
